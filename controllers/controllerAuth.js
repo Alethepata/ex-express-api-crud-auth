d179c6aa17363523b5a65f9174de24c496eea95e
Alethepata <alessia.patane@gmail.com>
2024-06-10 16:18:35 +0200

    edit login

--- controllers/controllerAuth.js
+++ controllers/controllerAuth.js
@@ -1,11 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const passwordHased = require('../utils/passwordHased.js');
+const { passwordHased, comparePassword } = require('../utils/password.js');
 
 const generateToken = require('../utils/generateToken.js');
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
     try {
         const { email, name, password } = req.body;
     
@@ -34,8 +34,39 @@ const register = async (req, res) => {
     }
 }
 
-const login = () => {
-    
+const login = async (req, res, next) => {
+    const error = 'Email o password errati' ;
+    try {
+        const { email, password } = req.body;
+        
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            return res.status(400).json({ status: 400, error: error })
+        }
+
+        const validPassword = await comparePassword(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({ error: error })
+        }
+
+        const token = generateToken({
+            email,
+            name: user.name
+        });
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            token
+        });
+
+    } catch (error) {
+        next(error); 
+    }
 }
 
 module.exports = {
