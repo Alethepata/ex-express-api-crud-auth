92291c72bf5c5df5d177945cde94b89042444568
Alethepata <alessia.patane@gmail.com>
2024-06-10 15:30:48 +0200

    edit register

--- controllers/controllerAuth.js
+++ controllers/controllerAuth.js
@@ -1,5 +1,37 @@
-const register = () => {
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const passwordHased = require('../utils/passwordHased.js');
+
+const generateToken = require('../utils/generateToken.js');
+
+const register = async (req, res) => {
+    try {
+        const { email, name, password } = req.body;
     
+        const data = {
+            email,
+            password: await passwordHased(password),
+            name
+        }
+
+    
+        const user = await prisma.user.create({ data });
+
+        const token = generateToken({
+            email,
+            name
+        });
+    
+        res.json({
+            name: user.name,
+            email: user.email,
+            token
+        });
+        
+    } catch (error) {
+        next(error);
+    }
 }
 
 const login = () => {
